3a859dd45bcc7254da052ede8c2ed20058caddb5
uuupppsss <ulya.ch.06@mail.ru>
2025-01-23 13:55:50 +1000

    gfbf

--- KokoytoUjas/Controllers/v1Controller.cs
+++ KokoytoUjas/Controllers/v1Controller.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using KokoytoUjas.Model;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace KokoytoUjas.Controllers
 {
@@ -8,11 +11,52 @@ namespace KokoytoUjas.Controllers
     [ApiController]
     public class v1Controller : ControllerBase
     {
-        [HttpPost("signin")]
-        public void SignIn(User user)
+        List<User> _users = new List<User>()
         {
+            new User()
+            {
+                Id = 1,
+                Login="паша",
+                Password="пашаadmin",
+                Role="admin"
+            }
+        };
 
+        [HttpPost("signin")]
+        public ActionResult<ResponseTokenAndRole> SignIn(User user)
+        {
+            User? found_user = _users.FirstOrDefault(u => u.Login == user.Login && u.Password == user.Password);
+            if (found_user is null)
+                return Unauthorized();
+           
+            string role = found_user.Role;
+            string username = found_user.Login;
+            int id = found_user.Id;
+
+            var claims = new List<Claim> {
+                //Id (если нужно)
+                new Claim(ClaimValueTypes.Integer32, id.ToString()),
+                //роль
+                new Claim(ClaimTypes.Role, role),
+                //юз
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            // создаем
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    claims: claims,
+                    expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+            string token = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return Ok(new ResponseTokenAndRole
+            {
+                Token = token,
+                Role = role
+            });
         }
     }
-
 }
